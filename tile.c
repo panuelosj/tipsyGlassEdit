d9e03d40d2bfd6258dbd28e23a0b269d7c3b9ed1
panuelosj <jonathan.panuelos@gmail.com>
2016-06-02 00:30:08 -0400

    add cleanup using tipsyDestroy

--- tile.c
+++ tile.c
@@ -59,4 +59,6 @@ int main(){
     printf("=================================================\n");
     writeTipsyStd("sodShocktube.std", sodShocktube);
 
+    // Cleanup
+    tipsyDestroy(glassIn); tipsyDestroy(glass8f); tipsyDestroy(glass8x); tipsyDestroy(sodShocktube);
 }
